bac2d661806b819b332b5df6bb62a76f7c55864f
achintyasrivastav <rachinka09@gmail.com>
2025-08-14 12:57:23 +0530

    Moodboard Project Completed

--- server/index.js
+++ server/index.js
@@ -3,11 +3,21 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import moodRoutes from "./routes/moodRoutes.js";
+import cors from "cors"; // Import the cors package
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Enable CORS
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow only requests from this origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  credentials: true, // Allow credentials (cookies, headers, etc.)
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Health check
@@ -16,7 +26,7 @@ app.get("/", (req, res) => res.send("API is running..."));
 // Auth routes
 app.use("/api/auth", authRoutes);
 
-//MoodBoard routes
+// MoodBoard routes
 app.use("/api/mood", moodRoutes);
 
 const PORT = process.env.PORT || 5000;
